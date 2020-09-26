80bbc4734d993fee99395ba1a1f390ff08882893
Ramon Snellink <r.snellink@gmail.com>
2020-09-26 19:30:19 +0200

    level 12

---
+++ 03L12B01/script.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+
+let name = "Ramon";
+
+console.log(name);
+
+console.log("4" + "4");
+console.log(4 + 4);
+console.log(12 - 2);
+
+let age = 34;
+
+console.log(typeof age);
