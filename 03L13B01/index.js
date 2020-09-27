4dfe5d78c79aa409dc4f74f444522480c5552738
Ramon Snellink <r.snellink@gmail.com>
2020-09-27 10:51:33 +0200

    lvl 13 assignment 1

---
+++ 03L13B01/index.js
@@ -0,0 +1,21 @@
+const age = 14;
+const isFemale = false;
+const driverStatus = "bob";
+
+if (age >= 18) {
+  console.log("Je mag naar binnen");
+} else {
+  console.log("Je mag niet naar binnen");
+}
+
+if (isFemale) {
+  console.log("Je bent een vrouw");
+} else {
+  console.log("Je bent geen vrouw");
+}
+
+if (driverStatus === "bob") {
+  console.log("Je mag rijden");
+} else {
+  console.log("Je mag niet rijden");
+}
